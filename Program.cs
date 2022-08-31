0e8ce6981afd30c6e13a5c81e5f1f43986517772
DLindeblom <drdlindeblom@gmail.com>
2022-08-31 15:38:14 -0400

    write method to calculate area of rectangle

--- Program.cs
+++ Program.cs
@@ -6,8 +6,19 @@ namespace ArchitectArithmetic
   {
     public static void Main(string[] args)
     {
+      double length = 36;
+      double width = 22;
+      
+      Console.Write($"The area of a rectangle with a length of {length}ft and a width of {width}ft is: ");
+      Console.Write(areaOfRectangle(length, width));
+      Console.WriteLine("ft squared.");
 
+    }
 
+    static double areaOfRectangle(double length, double width)
+    {
+      double area = length * width;
+      return area;
     }
 
 
